3f17df10ce62aa924e10940f78ace9cdaaaed519
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2024-02-14 00:58:11 +0530

    project setup done

---
+++ Vacation-Rental-Platform/src/main/java/com/vacation/rental/VacationRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vacation.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationRentalPlatformApplication.class, args);
+	}
+
+}
